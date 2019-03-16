89e72deecad9d32f2ed1536b10ef50a7dad965fa
Mike Arpaia <mike@arpaia.co>
2019-03-16 01:46:36 -0600

    Add prometheus metrics exporter

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 """This file contains the API server for serving language detection requests"""
 
 from flask import Flask, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
 
 class Server:
     """Server is the class that implements the API server"""
@@ -9,12 +10,14 @@ class Server:
         self.port = port
 
         self.app = Flask(name)
-        self.app.add_url_rule("/", "index", view_func=self.index)
+        self.app.add_url_rule("/api/v1/language/predict", "predict", view_func=self.predict)
+
+        self.metrics = PrometheusMetrics(self.app)
 
     def run(self):
         """start the server"""
         self.app.run(host=self.host, port=self.port)
 
-    def index(self):
-        """The base URL on the server"""
+    def predict(self):
+        """The endpoint for making predictions"""
         return jsonify({"port": self.port})
