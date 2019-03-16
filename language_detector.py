d97f6bebbdc08645a9c87f2558c6dc4ad0d32543
Mike Arpaia <mike@arpaia.co>
2019-03-16 16:39:33 -0600

    Simplify directory structure

--- language_detector.py
+++ language_detector.py
@@ -15,10 +15,33 @@ import argparse
 import sys
 
 import fastText
-from flask import Flask
+from flask import Flask, abort, jsonify, request
 from prometheus_flask_exporter import PrometheusMetrics
 
-from api.server import PredictionEndpoint
+class PredictionEndpoint():
+    """The endpoint for making predictions"""
+
+    def __init__(self, model):
+        self.model = model
+
+    def predict(self, text):
+        """Make a language prediction"""
+        prediction = self.model.predict(text)
+        language_code = prediction[0][0].replace("__label__", "")
+        return language_code
+
+    def endpoint(self):
+        """The endpoint implementation"""
+        req = request.get_json()
+        text = ""
+        try:
+            text = req["text"]
+        except KeyError:
+            abort(400)
+
+        return jsonify({
+            "language": self.predict(text),
+        })
 
 def main():
     """The command entrypoint"""
